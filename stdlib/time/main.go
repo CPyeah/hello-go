fbb74cb7d7b2d2c046fd6dd448da08d831ed70bf
chengpeng <chengpengyeah@126.com>
2022-07-16 16:21:58 +0800

    time

--- stdlib/time/main.go
+++ stdlib/time/main.go
@@ -8,6 +8,57 @@ import (
 func main() {
 	durationOperation()
 	locationOperation()
+	timeOperation()
+	tickerOperation()
+	timerOperation()
+
+}
+
+func timerOperation() {
+	var timer = time.NewTimer(time.Second)
+
+	select {
+	case t := <-timer.C:
+		fmt.Println("time is", t)
+	}
+
+	time.AfterFunc(time.Second, func() {
+		fmt.Println("delay one second function")
+	})
+
+	time.Sleep(time.Second * 2)
+}
+
+func tickerOperation() {
+	var tic = time.NewTicker(time.Second)
+	var after5seconds = time.Now().Add(time.Second * 5)
+
+ticLab:
+	for true {
+		select {
+		case t := <-tic.C:
+			if time.Now().After(after5seconds) {
+				break ticLab
+			}
+			fmt.Println(t)
+		}
+	}
+
+	tic.Reset(time.Hour)
+	tic.Stop()
+
+}
+
+func timeOperation() {
+	var now = time.Now()
+	var thousandDay = time.Hour * 24 * 1000
+	var fallInLoveTime, _ = time.Parse("2006-01-02 15:04:05", "2019-10-13 20:00:00")
+	fmt.Println(now, thousandDay, fallInLoveTime)
+
+	fmt.Println("thousand day:", fallInLoveTime.Add(thousandDay))
+	fmt.Println("still now:", now.Sub(fallInLoveTime).Hours()/24)
+	fmt.Println("now is", now.Format("2006-01-02 15:04:05"))
+
 }
 
 func locationOperation() {
