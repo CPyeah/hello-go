712babddd74099b73b4f14736a4c6b558395bd3a
chengpeng <chengpengyeah@126.com>
2022-07-20 21:59:37 +0800

    selectionSort

--- algorithm/sort.go
+++ algorithm/sort.go
@@ -14,3 +14,22 @@ func bubbleSort(nums []int) []int {
 	}
 	return nums
 }
+
+func selectionSort(nums []int) []int {
+
+	if len(nums) < 2 {
+		return nums
+	}
+	for i := 0; i < len(nums)-1; i++ {
+		var index = len(nums) - 1 - i
+		var maxValue = nums[index]
+		for j := 0; j < index; j++ {
+			if nums[j] > maxValue {
+				maxValue = nums[j]
+				Swap(nums, index, j)
+			}
+		}
+	}
+	return nums
+
+}
--- algorithm/sort_test.go
+++ algorithm/sort_test.go
@@ -24,3 +24,23 @@ func Test_bubbleSort(t *testing.T) {
 		})
 	}
 }
+
+func Test_selectionSort(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"selection-test", args{[]int{4, 3, 2, 5, 6, 1, 7}}, []int{1, 2, 3, 4, 5, 6, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := selectionSort(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("selectionSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
