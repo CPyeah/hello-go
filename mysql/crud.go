4120cc60d2727697e14f5ab190047f1333a68332
chengpeng <chengpengyeah@126.com>
2022-07-27 15:16:22 +0800

    mysql crud

--- mysql/crud.go
+++ mysql/crud.go
@@ -28,9 +28,81 @@ func main() {
 	defer closeMySQL()
 
 	queryCustomerById(1)
+
+	queryCustomers()
+
+	var id = insertCustomer("tom")
+	queryCustomerById(id)
+
+	updateCustomer(id, "jerry")
+	queryCustomerById(id)
+
+	deleteByName("jerry")
+	queryCustomers()
+
+	transaction()
+	queryCustomers()
 }
 
-func queryCustomerById(id int) {
+func transaction() {
+	var tx, err = mysql.Begin()
+	if err != nil {
+		panic(err)
+	}
+	var _, err1 = tx.Exec("insert into customer values (null, ?)", "Tom")
+	var _, err2 = tx.Exec("insert into customer values (null, ?)", "Jerry")
+
+	if err1 != nil || err2 != nil {
+		var _ = tx.Rollback()
+	} else {
+		var _ = tx.Commit()
+	}
+}
+
+func deleteByName(name string) {
+	var result, err = mysql.Exec("delete from customer where name = ?", name)
+	if err != nil {
+		panic(err)
+	}
+	var count, _ = result.RowsAffected()
+	fmt.Println("deleted", count, "line data.")
+}
+
+func updateCustomer(id int64, newName string) {
+	var result, err = mysql.Exec("update customer set name = ? where id = ?", newName, id)
+	if err != nil {
+		panic(err)
+	}
+	var count, _ = result.RowsAffected()
+	fmt.Println("updated", count, "line data")
+}
+
+func insertCustomer(name string) int64 {
+	result, err := mysql.Exec("insert into customer values (null, ?)", name)
+	if err != nil {
+		panic(err)
+	}
+	var effectRowsCount, _ = result.RowsAffected()
+	fmt.Println("inserted", effectRowsCount, "line data")
+	var id, _ = result.LastInsertId()
+	return id
+}
+
+func queryCustomers() {
+	var rows, err = mysql.Query("select * from customer limit 10")
+	if err != nil {
+		panic(err)
+	}
+	var customers []customer
+	for rows.Next() {
+		var customer customer
+		var _ = rows.Scan(&customer.id, &customer.name)
+		customers = append(customers, customer)
+	}
+	fmt.Println("customers list is", customers)
+}
+
+func queryCustomerById(id int64) {
 	var row = mysql.QueryRow("select * from customer where id = ?", id)
 	var customer customer
 	var err = row.Scan(&customer.id, &customer.name)
