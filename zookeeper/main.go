e6b36b153bb98cd4647ff3f41a772e11c4cb553c
chengpeng <chengpengyeah@126.com>
2022-08-11 15:57:17 +0800

    watch and lock

--- zookeeper/main.go
+++ zookeeper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-zookeeper/zk"
+	"sync"
 	"time"
 )
 
@@ -23,11 +24,70 @@ func main() {
 
 	tree("/hello", "-")
 
+	watch("/hello")
+
 	modify("/hello")
 
 	deleteChildren("/hello")
 
 	tree("/hello", "-")
+
+	lock()
+
+}
+
+func lock() {
+	var count = 0
+	lock := zk.NewLock(conn, "/hello/lock2", zk.WorldACL(zk.PermAll))
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := lock.LockWithData([]byte("locked"))
+			handleErr(err)
+			defer func(lock *zk.Lock) {
+				err := lock.Unlock()
+				handleErr(err)
+			}(lock)
+			count++
+		}()
+	}
+	wg.Wait()
+	fmt.Println("count is", count)
+}
+
+func watch(path string) {
+	go func() {
+		data, _, ch, err := conn.GetW(path)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("The current data is: %v.\nI'm watching.\n", string(data[:]))
+		for event := range ch {
+			switch event.Type {
+			case 1:
+				fmt.Printf("Node created.")
+				// Do somthing
+			case 2:
+				fmt.Printf("Node deleted!")
+				// Do somthing
+			case 3:
+				fmt.Printf("Node changed.")
+				// Do somthing
+			case 4:
+				fmt.Printf("Node children changed.")
+				// Do somthing
+			default:
+				fmt.Printf("Something happen.")
+			}
+			// Or just ...
+			fmt.Println("event type is ", event.Type.String())
+
+		}
+	}()
+
+	modify(path)
 }
 
 func deleteChildren(path string) {
